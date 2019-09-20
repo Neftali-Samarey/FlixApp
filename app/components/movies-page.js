742bb7d52d28629de88a45b939e61aa722cdf27b
Neftali-Samarey <neftali.quintero@cix.csi.cuny.edu>
2019-09-20 13:11:12 -0400

    Movie item class enabled

--- app/components/movies-page.js
+++ app/components/movies-page.js
@@ -1,4 +1,39 @@
 import Component from '@ember/component';
 
+
+class MovieObject {
+  constructor(title, movieDescription, runtime, rating, searchKeyword) {
+    this.title = title;
+    this.description = movieDescription;
+    this.runtime = runtime;
+    this.rating = rating;
+    this.keyword = searchKeyword;
+  }
+
+  // Setters and getters for the movie that we are constructing
+  get movieTitle() {
+    return this._movieTitle;
+  }
+  set movieTitle(value) {
+    this._movieTitle = value;
+  }
+ 
+
+}
+
 export default Component.extend({
+
+  model() {
+    return sampleMovies;
+  }
 });
+
+
+
+function sampleMovies() {
+
+  // Build a movie object using the class above
+  
+
+  return ("The Dark Knight", "Home Alone 5", "The Internship");
+}
