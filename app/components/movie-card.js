7c5c3e717581e5fbbbcd0d5c74a0b86e8afeb335
Neftali-Samarey <neftali.quintero@cix.csi.cuny.edu>
2019-09-26 12:13:06 -0400

    Updates

--- app/components/movie-card.js
+++ app/components/movie-card.js
@@ -1,5 +1,25 @@
 import Component from '@ember/component';
 
+class MovieItem {
+
+  constructor(title, year) {
+    this._title = title;
+    this._year = year;
+  }
+
+  get movieTitle() {
+    return this._title;
+  }
+
+  set title(title) {
+    this_title = title;
+  }
+
+  // Other operations
+
+
+}
+
 export default Component.extend({
-    
+
 });
